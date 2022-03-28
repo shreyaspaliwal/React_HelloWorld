481f50b18c4d1d3274fde738bf302ee0eb812382
Shreyas Paliwal <shreyaspaliwal21@gmail.com>
2022-03-28 22:04:35 +0530

    Added posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Posts from "./components/Posts";
 
 function App() {
   return (
@@ -9,15 +10,9 @@ function App() {
         <p>
           Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <Posts/>
     </div>
   );
 }
